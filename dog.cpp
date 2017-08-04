2d48d58add5dc4fb61bfbc8bca404a7a7c91ecff
= <drainwa1@vols.utk.edu>
2017-08-04 14:58:38 -0400

    Add file reading mode

--- dog.cpp
+++ dog.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <string>
+#include <fstream>
 
 void mirror_input() {
     std::string input;
@@ -8,14 +9,27 @@ void mirror_input() {
     }
 }
 
-void output_files() {
-    std::cout << "Output mode" << std::endl;
+void output_files(int num_files, char** file_names) {
+    
+    for (int i = 1; i <= num_files; i++) {
+        std::ifstream to_read(file_names[i]);
+        std::string line;
+
+        if (to_read.is_open()) {
+            while (std::getline(to_read, line)) {
+                std::cout << line << std::endl;
+            }
+            to_read.close();
+        } else {
+            std::cout << "dog: " << file_names[i] << ": No such file or directory" << std::endl;
+        }
+    }
 }
 
 int main (int argc, char** argv) {
     if (argc == 1) {
         mirror_input();
     } else {
-        output_files();
+        output_files(argc - 1, argv);
     }
 }
