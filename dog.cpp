0981bc871125bf58259ea4f852ca8b8ffbcb2d32
= <drainwa1@vols.utk.edu>
2017-08-04 20:42:58 -0400

    Add ability to read flags

--- dog.cpp
+++ dog.cpp
@@ -2,6 +2,9 @@
 #include <string>
 #include <fstream>
 
+int number_line_flag = 0; /* Determines whether or not to display line numbers */
+int word_count_flag  = 0; /* Determines whether or not to display word count */
+
 void mirror_input() {
     std::string input;
     while (std::getline(std::cin, input)) {
@@ -9,9 +12,8 @@ void mirror_input() {
     }
 }
 
-void output_files(int num_files, char** file_names) {
-    
-    for (int i = 1; i <= num_files; i++) {
+void output_files(int start_index, int num_files, char** file_names) {
+    for (int i = start_index; i <= num_files; i++) {
         std::ifstream to_read(file_names[i]);
         std::string line;
 
@@ -30,6 +32,26 @@ int main (int argc, char** argv) {
     if (argc == 1) {
         mirror_input();
     } else {
-        output_files(argc - 1, argv);
+        int start_index = 1;
+        for (int i = 1; i < argc - 1; i++) {
+            if (argv[i][0] == '-' && sizeof(argv[i]) > 1) { /* Detect flags */
+                start_index++;
+                switch (argv[i][1]) {
+                    case 'n':
+                        number_line_flag = 1;
+                        break;
+                    case 'c':
+                        word_count_flag = 1;
+                        break;
+                    default:
+                        std::cerr << "Invalid flag: " << argv[i] << std::endl;
+                        break;
+                }
+            } else {
+                std::cerr << "Invalid flag: " << argv[i] << std::endl;
+                return -1;
+            }
+        }
+        output_files(start_index, argc - 1, argv);
     }
 }
